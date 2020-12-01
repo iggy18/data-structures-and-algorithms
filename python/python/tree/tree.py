6e14a40a3a0abc3a497f3b3808dbc950e91bcb79
seth mcfeeters <seth.mcfeeters@gmail.com>
2020-12-01 07:59:48 -0800

    finished trees

--- python/python/tree/tree.py
+++ python/python/tree/tree.py
@@ -1,44 +1,88 @@
 class Node:
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
 
-new_node = Node(10)
-
 
 class BinaryTree:
-
-    def __init__(self, root):
+    def __init__(self):
         self.root = None
 
-    def preorder(self, start):
+    def preorder(self):
         """
-        this method checks the the root, then left, then right
+        this method checks the root, then left, then right
         """
-        pass
+        values = []
+
+        def walk(root):  # recursive
+            if not root:
+                return
+            # you can rearrange the steps below to get the other traversals
+            values.append(root.value)
+            walk(root.left)  # recursive
+            walk(root.right)  # recursive
+        walk(self.root)
+        print(values)
 
     def in_order(self):
         """
         this method checks the left, then root, then right.
         """
-        pass
+        values = []
+
+        def walk(root):  # recursive
+            if not root:
+                return
+            # you can rearrange the steps below to get the other traversals
+            walk(root.left)  # recursive
+            values.append(root.value)
+            walk(root.right)  # recursive
+        walk(self.root)
+        print(values)
 
     def post_order(self):
         """
         this method checks the left, then right, root.
         """
-        pass
+        values = []
+
+        def walk(root):  # recursive
+            if not root:
+                return
+            # you can rearrange the steps below to get the other traversals
+            walk(root.left)  # recursive
+            walk(root.right)  # recursive
+            values.append(root.value)
+        walk(self.root)
+        print(values)
 
 
-class BinarySearchTree:
+class BinarySearchTree(BinaryTree):
 
     def __init__(self):
         self.root = None
 
-    def add (self, data, node):
+    def add(self, value):
         pass
 
     def contains(self):
         pass
+
+
+a = Node("A")  # this assigns value to the node
+b = Node("B")
+c = Node("C")
+d = Node("D")
+e = Node("E")
+
+tree = BinaryTree()  # this makes the binary tree
+tree.root = a
+a.left = b
+a.right = c
+a.left.left = d
+a.left.right = e
+tree.preorder()
+tree.in_order()
+tree.post_order()
