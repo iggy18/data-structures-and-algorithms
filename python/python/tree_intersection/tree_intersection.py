3a4e6674a4ac622db1c6fb2ccd138a8ecd144646
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-01-27 09:56:20 -0800

    reversed array all tests passing

---
+++ python/python/tree_intersection/tree_intersection.py
@@ -0,0 +1,13 @@
+from .stacks.py import Queue
+from tree import BinaryTree
+from random import randint
+
+
+def tree_intersection(treeOne, treeTwo):
+    identical = []
+    tree_one = TreeOne.pre_order()
+    tree_two = TreeTwo.pre_order()
+    for potential_identical in tree_one:
+        if potential_identical in tree_two:
+            identical.append(potential_identical)
+    return identical
