a82a147b6e5cfb23212fb87590f2e01029d0148d
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-01-28 17:10:39 -0800

    test for diferent length lists passing

--- python/python/linked_list/test_merge.py
+++ python/python/linked_list/test_merge.py
@@ -16,3 +16,21 @@ def test_insert_multiple_node():
     actual = z.contains()
     expected =  ["f","e","d","c","b","a"]
     assert actual == expected
+
+def test_dif_length_lists():
+    x = LinkedList()
+    x.insert("4")
+    x.insert("3")
+    x.insert("2")
+    x.insert("1")
+    x.insert("a")
+    x.insert("c")
+    x.insert("e")
+    y = LinkedList()
+    y.insert("b")
+    y.insert("d")
+    y.insert("f")
+    z = zip_lists(x, y)
+    actual = z.contains()
+    expected =  ["f","e","d","c","b","a","1","2","3","4"]
+    assert actual == expected
