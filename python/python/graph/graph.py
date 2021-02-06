da55a0403bec45520499cb52aefe5405696d494d
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-06 12:35:18 -0800

    all tests passing for breadth first traversal of graph

--- python/python/graph/graph.py
+++ python/python/graph/graph.py
@@ -1,3 +1,5 @@
+from stacks import Queue
+
 class WhatAreYouDoing(Exception):
     pass
 
@@ -16,16 +18,15 @@ class Graph:
         self.neighbors = {}
 
     def add_node(self, val):
-        node = Vertex(val)
-        self.neighbors[node] = []
-        return node
+        vertex = Vertex(val)
+        self.neighbors[vertex] = []
+        return vertex
 
     def add_edge(self, origin, destination, weight=0):
         if origin not in self.neighbors:
-            raise WhatAreYouDoing('you need an origin node')
+            raise WhatAreYouDoing('you need an origin vertex')
         if destination not in self.neighbors:
-            raise WhatAreYouDoing('you need a destination node')
-
+            raise WhatAreYouDoing('you need a destination vertex')
         connect = Edge(destination, weight)
         connections = self.neighbors[origin]
         connections.append(connect)
@@ -33,8 +34,22 @@ class Graph:
     def get_nodes(self):
         return self.neighbors.keys()
 
-    def get_neighbors(self, node):
-        return self.neighbors[node]
+    def get_neighbors(self, vertex):
+        return self.neighbors[vertex]
 
     def size(self):
         return len(self.neighbors)
+
+    def breadth_first(self, vertex):
+        visited_vertex = [vertex]
+        queue = Queue()
+        queue.enqueue(vertex)
+        while not queue.is_empty():
+            vertex = queue.dequeue()
+            neighbors = [edge.vertex for edge in self.get_neighbors(vertex)]
+            for neighbor in neighbors:
+                if neighbor not in visited_vertex:
+                    visited_vertex.append(neighbor)
+                    queue.enqueue(neighbor)
+        return visited_vertex
+
--- python/python/graph/test_graph.py
+++ python/python/graph/test_graph.py
@@ -120,3 +120,29 @@ def test_size_two():
     expected = 2
     actual = d.size()
     assert actual == expected
+
+def test_breadth_first():
+    g = Graph()
+    pandora = g.add_node("Pandora")
+    arendelle = g.add_node("Arendelle")
+    metroville = g.add_node("Metroville")
+    monstropolis = g.add_node("Monstropolis")
+    narnia = g.add_node("Narnia")
+    naboo = g.add_node("Naboo")
+    g.add_edge(pandora, arendelle)
+    g.add_edge(arendelle, pandora)
+    g.add_edge(arendelle, metroville)
+    g.add_edge(metroville, arendelle)
+    g.add_edge(arendelle, monstropolis)
+    g.add_edge(monstropolis, arendelle)
+    g.add_edge(metroville, monstropolis)
+    g.add_edge(monstropolis, metroville)
+    g.add_edge(metroville, narnia)
+    g.add_edge(narnia, metroville)
+    g.add_edge(metroville, naboo)
+    g.add_edge(naboo, metroville)
+    g.add_edge(narnia, naboo)
+    g.add_edge(naboo, narnia)
+    vertices = g.breadth_first(pandora)
+    values = [vertex.val for vertex in vertices]
+    assert values == ["Pandora", "Arendelle"]
