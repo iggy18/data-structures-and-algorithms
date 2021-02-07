40e3f6df1f4cf6302a93f2383482045cacd03943
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-07 14:27:53 -0800

    created tests for reverse string. all tests fail because the functions haven't been created yet. next commit will have working functions and passing tests

--- python/python/reverse_string_3_ways/test_reverse.py
+++ python/python/reverse_string_3_ways/test_reverse.py
@@ -8,3 +8,21 @@ def test_reverse_two():
 
 def test_reverse_three():
     assert reverse_three
+
+def test_reverse_one_reverses_string():
+    x = "hello there"
+    actual = reverse_one(x)
+    expected = "ereht olleh"
+    assert actual == expected
+
+def test_reverse_two_reverses_string():
+    x = "hello there"
+    actual = reverse_two(x)
+    expected = "ereht olleh"
+    assert actual == expected
+
+def test_reverse_three_reverses_string():
+    x = "hello there"
+    actual = reverse_three(x)
+    expected = "ereht olleh"
+    assert actual == expected
