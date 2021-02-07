b5848807eb77971704cbc9567ef9a57ac550e317
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-07 14:51:39 -0800

    all tests passing for reverse three ways

--- python/python/hashtable/test_hashtable.py
+++ python/python/hashtable/test_hashtable.py
@@ -26,20 +26,3 @@ def test_different_hash():
     initial = hashtable.hash('glisten')
     secondary = hashtable.hash('silent')
     assert initial != secondary
-
-
-def test_get_apple():
-    hashtable = HashTable()
-    hashtable.set("apple", "Used for apple sauce")
-    actual = hashtable.get("apple")
-    expected = "Used for apple sauce"
-    assert actual == expected
-
-
-def test_get_silent_and_listen():
-    hashtable = HashTable()
-    hashtable.set('listen', 'to me')
-    hashtable.set('silent', 'so quiet')
-
-    assert hashtable.get('listen') == 'to me'
-    assert hashtable.get('silent') == 'so quiet'
