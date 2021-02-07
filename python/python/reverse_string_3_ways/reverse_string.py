fc8a6903aeb27bc8feb442781d75c70a746f3f95
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-07 14:21:11 -0800

    init commit for reverse string

---
+++ python/python/reverse_string_3_ways/reverse_string.py
@@ -0,0 +1,29 @@
+def reverse_one(string):
+    pass
+
+def reverse_two(string):
+    pass
+
+def reverse_three(string):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python/python/reverse_string_3_ways/test_reverse.py
@@ -0,0 +1,10 @@
+from reverse_string import reverse_one, reverse_two, reverse_three
+
+def test_reverse_one():
+    assert reverse_one
+
+def test_reverse_two():
+    assert reverse_two
+
+def test_reverse_three():
+    assert reverse_three
