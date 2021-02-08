fad476eb80813dbd30801311d32f786b9a17de0c
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-08 09:00:01 -0800

    made tests for insertion sort and function that checks sorting is correct, time to code insertion sort

--- python/python/insertion_sort/insertion_sort.py
+++ python/python/insertion_sort/insertion_sort.py
@@ -1,3 +1,12 @@
+def check_sort(arr):
+    prev = float('-inf')
+    for num in arr:
+        if num < prev:
+            return False
+        if num >= prev:
+            prev = num
+    return True
+
 def insertion_sort(arr):
     pass
 
