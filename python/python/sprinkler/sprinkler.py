8b73ec50b78ab562e186b04a5a27f8cfe7561f11
seth mcfeeters <seth.mcfeeters@gmail.com>
2021-02-07 17:14:46 -0800

    all tests passing, added problem domain and algo

--- python/python/sprinkler/sprinkler.py
+++ python/python/sprinkler/sprinkler.py
@@ -1,3 +1,16 @@
+# Problem domain: Estimate water usage for a sprinkler system.The sprinkler system is made of an inlet and a number of connected sprinkler heads that spray water. Each single spinkler head has a T connection that can be connected to 0, 1, or 2 other sprinklers.Each sprinkler head is configured for a certain amount of output, each potentially different, in millileters per minute
+
+'''
+algo:
+create empty array for node values
+create helper function that takes in systems root
+add roots value to array
+recurse to the left line
+recurse to the right line
+return the sum of the values in the array
+'''
+
+
 class Sprinkler:
     def __init__(self, usage=2):
         self.usage = usage
